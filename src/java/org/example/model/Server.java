ab9f54f99fa73aaa09aa897d72a7b26197b9980b
cgalino <carlesgalino@gmail.com>
2016-05-03 10:40:31 +0200

    :o:

--- src/java/org/example/model/Server.java
+++ src/java/org/example/model/Server.java
@@ -5,11 +5,52 @@
  */
 package org.example.model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Carlos
  */
 public class Server {
+    private int id;
+    private String url;
+    private String name;
+    private ArrayList<User> members = new ArrayList<>();
+    
+    public Server() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<User> getMembers() {
+        return members;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMembers(ArrayList<User> members) {
+        this.members = members;
+    }
     
     
 }
